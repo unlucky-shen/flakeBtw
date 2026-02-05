4b5e96eb92088d02c04559bdc5ccb8618abe3107
unlucky-shen <taufiqhaikal002@gmail.com>
2026-02-05 16:43:51 +0800

    added typst support

--- nixvim/plugins.nix
+++ nixvim/plugins.nix
@@ -11,39 +11,41 @@ plugins.mini-pick.enable = true;
 plugins.nvim-autopairs.enable = true;
 plugins.indent-blankline.enable = true;
 plugins.treesitter = {
-  enable = true;
-  settings = {
-  	auto_install = false;
-    ensure_installed = [
+	enable = true;
+	settings = {
+		auto_install = false;
+		ensure_installed = [
 			"r"
 			"python"
 			"lua"
 			"rust"
 			"c"
 			"cpp"
-      ];
-    highlight.enable = true;
-  };
+			"typst"
+			];
+		highlight.enable = true;
+	};
 };
 
 plugins.lsp = {
 	enable = true;
 	servers = {
 		clangd.enable = true;
+		tinymist.enable = true;
 		pyright = {
-        enable = true;
-        settings.python.analysis.typeCheckingMode = "off";
-    };
+			enable = true;
+			settings.python.analysis.typeCheckingMode = "off";
+		};
 		rust_analyzer = {
-        enable = true;
-        package = null;
-        installCargo = false;
-        installRustc = false;
-    };
+			enable = true;
+			package = null;
+			installCargo = false;
+			installRustc = false;
+		};
 		lua_ls = {
-        enable = true;
-        settings.Lua.diagnostic.disable = [ "missing-fields" ];
-    };
+			enable = true;
+			settings.Lua.diagnostic.disable = [ "missing-fields" ];
+		};
 		# r_language_server.enable = true;
 	};
 
