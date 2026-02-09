a293f8413786c99a877997306e11a2b54e10db71
Taufiq Haikal <taufiqhaikal002@gmail.com>
2026-02-09 19:51:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ git clone https://github.com/unlucky-shen/.FlakeBtw.git
 git clone https://github.com/unlucky-shen/hypr.git
 git clone https://github.com/unlucky-shen/kitty.git
 cd .FlakeBtw
-sudo nixos-rebuild switch --flake .
+sudo nixos-rebuild switch --flake .#Tau
 
